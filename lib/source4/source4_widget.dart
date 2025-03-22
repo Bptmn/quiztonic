fbe940f535eb5abd002f6e7101158ef4acb52606
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-22 00:47:47 +0000

    Updating to latest FlutterFlow output.

--- lib/source4/source4_widget.dart
+++ lib/source4/source4_widget.dart
@@ -1,4 +1,5 @@
 import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -57,6 +58,20 @@ class _Source4WidgetState extends State<Source4Widget> {
         appBar: AppBar(
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
+          leading: FlutterFlowIconButton(
+            borderColor: Colors.transparent,
+            borderRadius: 30.0,
+            borderWidth: 1.0,
+            buttonSize: 60.0,
+            icon: Icon(
+              Icons.arrow_back_rounded,
+              color: Colors.white,
+              size: 30.0,
+            ),
+            onPressed: () async {
+              context.pop();
+            },
+          ),
           title: Text(
             'Page Title',
             style: FlutterFlowTheme.of(context).headlineMedium.override(
@@ -67,7 +82,7 @@ class _Source4WidgetState extends State<Source4Widget> {
                 ),
           ),
           actions: [],
-          centerTitle: false,
+          centerTitle: true,
           elevation: 2.0,
         ),
         body: SafeArea(
