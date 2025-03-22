73296b13b6ec7cefa180bcfd204bd8cd5b1305f5
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-22 00:30:25 +0000

    Updating to latest FlutterFlow output.

--- lib/other_pages/source_page/source_page_widget.dart
+++ lib/other_pages/source_page/source_page_widget.dart
@@ -3,6 +3,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'dart:ui';
+import '/index.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -70,7 +71,7 @@ class _SourcePageWidgetState extends State<SourcePageWidget> {
               size: 30.0,
             ),
             onPressed: () async {
-              context.safePop();
+              context.goNamed(HomePageWidget.routeName);
             },
           ),
           title: Text(
