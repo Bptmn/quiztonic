f7666db3c3c3c8fbe46d457d47bf94d36020b232
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-05-17 09:35:06 +0000

    Updating to latest FlutterFlow output.

--- lib/settings_pages/subscription_page/subscription_page_model.dart
+++ lib/settings_pages/subscription_page/subscription_page_model.dart
@@ -1,7 +1,6 @@
 import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/backend/schema/enums/enums.dart';
-import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -13,7 +12,6 @@ import '/index.dart';
 import 'subscription_page_widget.dart' show SubscriptionPageWidget;
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
@@ -24,8 +22,6 @@ class SubscriptionPageModel extends FlutterFlowModel<SubscriptionPageWidget> {
 
   ///  State fields for stateful widgets in this page.
 
-  // Stores action output result for [Custom Action - revenueCatCurrentUserSubscription] action in SubscriptionPage widget.
-  UserCurrentSubscriptionStruct? currentSub;
   // State field(s) for CheckboxListTile widget.
   bool? checkboxListTileValue1;
   // State field(s) for CheckboxListTile widget.
